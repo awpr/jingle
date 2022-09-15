a3108a84488625732da9fb9f92b8e7b0d8950492
Andrew Pritchard <awpritchard@gmail.com>
2022-09-15 00:59:18 -0700

    Add a play subcommand to launch straight into a synthesizer.

--- app/Main.hs
+++ app/Main.hs
@@ -1,33 +1,68 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Main (main) where
 
 import Control.Applicative ((<**>))
+import Control.Exception (bracket)
+import System.Directory (removeFile)
+import System.IO (openBinaryTempFile, hClose)
+import System.Process (callProcess)
 
+import Data.ByteString.Lazy qualified as BL
 import Options.Applicative
   ( Parser, execParser, strArgument, help, metavar
+  , subparser, command
   , helper, info, fullDesc, progDesc, header
   )
+import Sound.MIDI.File.Save qualified as MIDI
 
-import Jingle.Driver (compileFile)
+import Jingle.Driver (compileToFile, compileToMIDI)
 
-data Opts = Opts
-  { srcFile :: String
-  , dstFile :: String
-  }
 
-opts :: Parser Opts
-opts =
-  Opts
-    <$> strArgument (help "Source file" <> metavar "SRC")
-    <*> strArgument (help "Destination MIDI file" <> metavar "DST")
+srcFile :: Parser FilePath
+srcFile = strArgument (help "Source file" <> metavar "SRC")
+
+dstFile :: Parser FilePath
+dstFile = strArgument (help "Destination MIDI file" <> metavar "DST")
+
+data Action
+  = Compile FilePath FilePath
+  | Play FilePath
+
+action :: Parser Action
+action = subparser $ mconcat
+  [ command "compile" $
+      info (Compile <$> srcFile <*> dstFile <**> helper) $ mconcat
+        [ progDesc "Compile a jingle to MIDI."
+        ]
+  , command "play" $
+      info (Play <$> srcFile) $ mconcat
+        [ progDesc "Play a jingle via a synthesizer."
+        ]
+  ]
 
 main :: IO ()
 main = do
-  Opts{..} <- execParser $
-    info (opts <**> helper) $ mconcat
+  act <- execParser $
+    info (action <**> helper) $ mconcat
       [ fullDesc
-      , progDesc "Compile a jingle to MIDI"
       , header "jingle - a CLI tool for jingle music notation"
       ]
-  compileFile srcFile dstFile
+
+  case act of
+    Compile src dst -> compileToFile src dst
+    Play src ->
+      bracket
+        (openBinaryTempFile "/tmp" "jingle.midi")
+        (\ (name, h) -> hClose h >> removeFile name)
+        (\ (name, h) -> do
+          midi <- compileToMIDI src
+          BL.hPut h (MIDI.toByteString midi)
+          hClose h
+          callProcess "fluidsynth"
+            [ "-a", "alsa", "-m", "alsa_seq", "-l"
+            , "-i", "/usr/share/soundfonts/FluidR3_GM.sf2"
+            , name
+            ]
+          return ())
--- src/Jingle/Driver.hs
+++ src/Jingle/Driver.hs
@@ -1,20 +1,25 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 
-module Jingle.Driver (compileFile) where
+module Jingle.Driver (compileToMIDI, compileToFile) where
 
 import Control.Exception (throwIO)
 
 import Data.Text.IO qualified as T
 import Text.Megaparsec (parse, errorBundlePretty)
+import Sound.MIDI.File qualified as MIDI
+import Sound.MIDI.File.Save qualified as MIDI
 
 import Jingle.Parser (comp)
 import Jingle.Desugar (dsTrackContents)
-import Jingle.ToMIDI (writeMIDIFile)
+import Jingle.ToMIDI (toMIDIFile)
 
-compileFile :: FilePath -> FilePath -> IO ()
-compileFile src dst = do
+compileToMIDI :: FilePath -> IO MIDI.T
+compileToMIDI src = do
   contents <- T.readFile src
   ast <- case parse comp src contents of
     Left err -> throwIO $ userError $ errorBundlePretty err
     Right r -> pure r
-  writeMIDIFile dst $ fmap (dsTrackContents 0 Nothing) ast
+  return $ toMIDIFile $ fmap (dsTrackContents 1 Nothing) ast
+
+compileToFile :: FilePath -> FilePath -> IO ()
+compileToFile src dst = MIDI.toFile dst =<< compileToMIDI src
